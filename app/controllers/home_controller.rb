d55ca1a8e218520d327d901e7ee33c27e03b0442
robert morales <rmoralfit@gmail.com>
2022-06-03 18:43:38 -0400

    adding home/zipcode controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=07302&distance=0&API_KEY=61342EE3-66D1-417E-AAD6-5F47461D6053'
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89108&distance=0&API_KEY=61342EE3-66D1-417E-AAD6-5F47461D6053'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
@@ -37,4 +37,8 @@ class HomeController < ApplicationController
         @api_description = "Current Air Quality (301-500) Hazardous"
     end
   end
+
+    def zipcode
+
+    end
 end
