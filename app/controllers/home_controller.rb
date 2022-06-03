d21ecc6c8dd92a3852dbcf47dd3378585e7a82bf
robert morales <rmoralfit@gmail.com>
2022-06-02 20:08:26 -0400

    adding air quality messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,28 @@ class HomeController < ApplicationController
         @final_output = "Error"
     else
         @final_output = @output[0]['AQI']
-    
+    end
+
+    if @final_output == "Error"
+        @api_color = "silver"
+    elsif @final_output <= 50
+        @api_color = "green"
+        @api_description = "Current Air Quality Good"
+    elsif @final_output >= 51 && @final_output <= 100
+        @api_color = "yellow"
+        @api_description = "Current Air Quality Moderate"
+    elsif @final_output >= 101 && @final_output <= 150
+        @api_color = "orange"
+        @api_description = "Current Air Quality Unhealthy for Sensitive Groups"
+    elsif @final_output >= 151 && @final_output <= 200
+        @api_color = "red"
+        @api_description = "Current Air Quality Unhealthy"
+    elsif @final_output >= 201 && @final_output <= 300
+        @api_color = "purple"
+        @api_description = "Current Air Quality Very Unhealthy"
+    elsif @final_output >= 301 && @final_output <= 500
+        @api_color = "maroon"
+        @api_description = "Current Air Quality Hazardous"
     end
   end
 end
